3c44f91a64b3bc1845f97f55c14036ecbccc29cb
CM012 <charles20041209@gmail.com>
2024-09-30 11:35:30 +0800

    3rd_change

--- 4ndrecommender.py
+++ 4ndrecommender.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(description='The differences in intakes')
 parser.add_argument('f', type=float, help='Difference in the targeted fat intake') #Fat
 parser.add_argument('p', type=float, help='Difference in the targeted protein intake') #Protein
 parser.add_argument('c', type=float, help='Difference in the targeted carb intake') #Carbon
-parser.add_argument('-m', type=float, nargs='+', help='List of the margin') #margin
+parser.add_argument('-m', type=float, help='List of the margin') #margin
 parser.add_argument('-pf', type=int, nargs='+', help='List of the preference weight') #preference_weight
 args = parser.parse_args()
 #option = parser.parse_args()
@@ -38,12 +38,12 @@ sweetpotato = m.Var(lb=0) #x1
 redlentils = m.Var(lb=0) #x2
 avocado = m.Var(lb=0) #x3
 
-m.Equation(sweetpotato_nu[0]*sweetpotato + redlentils_nu[0]*redlentils + avocado_nu[0]*avocado <= carb + margin[2])
-m.Equation(sweetpotato_nu[0]*sweetpotato + redlentils_nu[0]*redlentils + avocado_nu[0]*avocado >= carb - margin[2]) 
-m.Equation(sweetpotato_nu[1]*sweetpotato + redlentils_nu[1]*redlentils + avocado_nu[1]*avocado <= protein + margin[2])
-m.Equation(sweetpotato_nu[1]*sweetpotato + redlentils_nu[1]*redlentils + avocado_nu[1]*avocado >= protein - margin[2])
-m.Equation(sweetpotato_nu[2]*sweetpotato + redlentils_nu[2]*redlentils + avocado_nu[2]*avocado <= fat + margin[2])
-m.Equation(sweetpotato_nu[2]*sweetpotato + redlentils_nu[2]*redlentils + avocado_nu[2]*avocado >= fat - margin[2])
+m.Equation(sweetpotato_nu[0]*sweetpotato + redlentils_nu[0]*redlentils + avocado_nu[0]*avocado <= carb + margin)
+m.Equation(sweetpotato_nu[0]*sweetpotato + redlentils_nu[0]*redlentils + avocado_nu[0]*avocado >= carb - margin) 
+m.Equation(sweetpotato_nu[1]*sweetpotato + redlentils_nu[1]*redlentils + avocado_nu[1]*avocado <= protein + margin)
+m.Equation(sweetpotato_nu[1]*sweetpotato + redlentils_nu[1]*redlentils + avocado_nu[1]*avocado >= protein - margin)
+m.Equation(sweetpotato_nu[2]*sweetpotato + redlentils_nu[2]*redlentils + avocado_nu[2]*avocado <= fat + margin)
+m.Equation(sweetpotato_nu[2]*sweetpotato + redlentils_nu[2]*redlentils + avocado_nu[2]*avocado >= fat - margin)
 
 m.Maximize(preference_weight[1]*sweetpotato + preference_weight[0]*redlentils + preference_weight[1]*avocado)
 #m.Maximize(x1 + x2 + x3)
